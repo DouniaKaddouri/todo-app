d234fc462eba932b1ee36b0b8b947c64052d2e01
Dounia Kaddouri <douniakadd04@gmail.com>
2025-01-29 16:29:16 +0100

    premier commit

---
+++ js-test/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './component/Count'
+import  TasksPage from './component/tasksPage'
+import './App.css'
+function App() {
+
+  return (
+    <div className='main'>
+     
+      <TasksPage />
+    </div>
+  )
+}
+
+export default App
